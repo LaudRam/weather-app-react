b9eaeabef7d16dc0731798d022145827039975eb
LaudRam <laudiciaramasenya@gmail.com>
2023-10-17 16:25:10 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SearchEngine from "./SearchEngine";
+import { RotatingLines } from "react-loader-spinner";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather app</h1>
+        <SearchEngine />
+        <RotatingLines
+          strokeColor="yellow"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="96"
+          visible={true}
+        />
       </header>
     </div>
   );
